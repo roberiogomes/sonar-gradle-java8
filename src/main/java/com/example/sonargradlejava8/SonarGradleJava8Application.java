b79f389f55564209b22091ff93e67d4d0619dbfd
Robério Gomes Patricio <roberiogomes@gmail.com>
2022-01-19 10:37:19 -0300

    Initial version

---
+++ src/main/java/com/example/sonargradlejava8/SonarGradleJava8Application.java
@@ -0,0 +1,13 @@
+package com.example.sonargradlejava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarGradleJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarGradleJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonargradlejava8/SonarGradleJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonargradlejava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarGradleJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
